2ff2ca77ca2c18f699b670fe3f8b963d885cde83
zhengziheng <741346146@qq.com>
2024-06-22 23:57:43 +0800

    update data

--- run_page/keep_to_strava_sync.py
+++ run_page/keep_to_strava_sync.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import os
-from sre_constants import SUCCESS
+# from sre_constants import SUCCESS
 import time
 from collections import namedtuple
 import requests
@@ -10,7 +10,7 @@ from Crypto.Cipher import AES
 from config import OUTPUT_DIR
 from stravalib.exc import ActivityUploadFailed, RateLimitTimeout
 from utils import make_strava_client, upload_file_to_strava
-from keep_sync import KEEP_DATA_TYPE_API, get_all_keep_tracks
+from keep_sync import get_all_keep_tracks
 from strava_sync import run_strava_sync
 
 """
@@ -73,12 +73,12 @@ if __name__ == "__main__":
 
     options = parser.parse_args()
     for api in options.sync_types:
-        assert (
-            api in KEEP_DATA_TYPE_API
-        ), f"{api} are not supported type, please make sure that the type entered in the {KEEP_DATA_TYPE_API}"
-    new_tracks = run_keep_sync(
-        options.phone_number, options.password, options.sync_types, True
-    )
+        # assert (
+        #     api in KEEP_DATA_TYPE_API
+        # ), f"{api} are not supported type, please make sure that the type entered in the {KEEP_DATA_TYPE_API}"
+        new_tracks = run_keep_sync(
+            options.phone_number, options.password, options.sync_types, True
+        )
 
     # to strava.
     print("Need to load all gpx files maybe take some time")
